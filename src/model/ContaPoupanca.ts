d5e11ee6ebae78a8284a0d95da1bcb1cdf7b47ed
mayaramonteiro <imayagmb@gmail.com>
2026-02-06 23:26:23 -0300

    :bug: corrigindo indentação

--- src/model/ContaPoupanca.ts
+++ src/model/ContaPoupanca.ts
@@ -1,38 +1,31 @@
-//Atributos chamado aniversario  vai sobrescrever o metodo visualizar o sacar nao, o resto nao muda nada
-
-import { Conta } from './Conta';
+import { Conta } from "./Conta";
 
 export class ContaPoupanca extends Conta {
+  private _aniversario: number;
 
-    private _aniversario: number;
+  constructor(
+    numero: number,
+    agencia: number,
+    titular: string,
+    tipo: number,
+    saldo: number,
+    aniversario: number,
+  ) {
+    super(numero, agencia, titular, tipo, saldo);
+    this._aniversario = aniversario;
+  }
 
-    constructor(
-        numero: number,
-        agencia: number, 
-        titular: string,
-        tipo: number, 
-        saldo: number, 
-        aniversario: number,) {
+  // Métodos GET e SET especificos da Classe ContaCPoupanca
+  public get aniversario(): number {
+    return this._aniversario;
+  }
 
-        
-        super(numero, agencia, titular, tipo, saldo);
-        this._aniversario = aniversario;
+  public set aniversario(value: number) {
+    this._aniversario = value;
+  }
 
-
-    }
-
-    // Métodos GET e SET especificos da Classe ContaCPoupanca
-    public get aniversario(): number {
-        return this._aniversario;
-    }
-
-    public set aniversario(value: number) {
-        this._aniversario = value;
-    }
-
-    public visualizar(): void {
-        super.visualizar();
-        console.log(`aniversario da Conta: ${this._aniversario}`);
-    }
-
+  public visualizar(): void {
+    super.visualizar();
+    console.log(`aniversario da Conta: ${this._aniversario}`);
+  }
 }
